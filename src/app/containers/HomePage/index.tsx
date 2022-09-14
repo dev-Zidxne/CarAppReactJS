5a7078afcf864347084eaf8f428c21c5b8a3cda1
Zidane Innis <zidaneinnis@gmail.com>
2022-09-14 04:06:12 -0500

    first commit!

---
+++ src/app/containers/HomePage/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components/macro";
+import tw from "twin.macro";
+
+const PageContainer = styled.div`
+  ${tw`flex flex-col items-center w-full h-full overflow-x-hidden`}
+`;
+
+export function HomePage() {
+  return <PageContainer>Hello world</PageContainer>;
+}
