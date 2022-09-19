5fc92b62a414c80df3e75352199cf1b7482b21f7
Zidane Innis <zidaneinnis@gmail.com>
2022-09-18 23:57:44 -0500

    Fixed styled component error

--- src/app/components/navbar/index.tsx
+++ src/app/components/navbar/index.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 import styled from "styled-components/macro";
 import tw from "twin.macro";
+import Logo from "../logo";
 
 const NavBarContainer = styled.div`
   min-height: 68px;
-  ${tw`flex flex-row items-center justify-between w-full max-w-2xl lg-pl-12 lg:pr-12 `}
+  ${tw`flex flex-row items-center justify-between w-full max-w-2xl lg:pl-12 lg:pr-12 `}
 `;
 
+const LogoContainer = styled.div``;
+
 function Navbar() {
-  return <NavBarContainer></NavBarContainer>;
+  return (
+    <NavBarContainer>
+      <LogoContainer>
+        <Logo />
+      </LogoContainer>
+    </NavBarContainer>
+  );
 }
 
 export default Navbar;
--- src/app/containers/HomePage/index.tsx
+++ src/app/containers/HomePage/index.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import styled from "styled-components/macro";
 import tw from "twin.macro";
+import Navbar from "../../components/navbar";
 
 const PageContainer = styled.div`
   ${tw`flex flex-col items-center w-full h-full overflow-x-hidden`}
 `;
 
 export function HomePage() {
-  return <PageContainer>Hello world </PageContainer>;
+  return (
+    <PageContainer>
+      <Navbar />
+    </PageContainer>
+  );
 }
