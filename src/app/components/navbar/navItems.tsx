9da91ee73b2bbce9695763bd240eb823620c92dd
Zidane Innis <zidaneinnis@gmail.com>
2022-09-19 21:43:05 -0500

    Adding styled components

--- src/app/components/navbar/navItems.tsx
+++ src/app/components/navbar/navItems.tsx
@@ -6,7 +6,7 @@ const ListContainer = styled.ul`
   ${tw`flex list-none `}
 `;
 const NavItem = styled.li`
-  ${tw``}
+  ${tw`mr-1 text-xs font-medium transition duration-300 ease-in-out cursor-pointer  md-text-base md:mr-1 hover:text-gray-700`}
 `;
 
 function navItems() {
