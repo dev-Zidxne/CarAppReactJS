8d69502c699798af8e52e9c8a517cb0f5ddaad64
Zidane Innis <zidaneinnis@gmail.com>
2022-09-19 21:40:50 -0500

    Created nav items component

---
+++ src/app/components/navbar/navItems.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
+
+const ListContainer = styled.ul`
+  ${tw`flex list-none `}
+`;
+const NavItem = styled.li`
+  ${tw``}
+`;
+
+function navItems() {
+  return <div>navItems</div>;
+}
+
+export default navItems;
