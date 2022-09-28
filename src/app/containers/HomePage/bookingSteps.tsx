bfde1c376b11cb320ca16dfb8b004fa8105214e2
Zidane Innis <zidaneinnis@gmail.com>
2022-09-27 23:57:29 -0500

    Minor tweaks

--- src/app/containers/HomePage/bookingSteps.tsx
+++ src/app/containers/HomePage/bookingSteps.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import tw from "twin.macro";
 
 const Container = styled.div`
-  ${tw`flex flex-col w-full `}
+  ${tw`flex flex-col w-full pt-3 pb-3 lg:pt-6 `}
 `;
 
 export function BookingSteps() {}
