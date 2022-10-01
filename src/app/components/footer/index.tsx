ccf445ddac23734d183dc370323ee1a2c9b2d68f
Zidane Innis <zidaneinnis@gmail.com>
2022-10-01 11:09:46 -0500

    Creating & Designing App Footer

---
+++ src/app/components/footer/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
+
+const FooterContainer = styled.div`
+  ${tw``}
+`;
+
+export function Footer() {
+  return <div>index</div>;
+}
