7504a47ddd3e857d8b228b555609b7a3204a0137
Zidane Innis <zidaneinnis@gmail.com>
2022-10-01 14:30:55 -0500

    Designing App footer

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -6,19 +6,30 @@ import Logo from "../logo";
 const FooterContainer = styled.div`
   min-height: 30em;
   background-color: rgb(31 42 76);
-  ${tw`flex flex-row flex-wrap min-w-full pb-1 pt-7`}
+  ${tw`flex items-center justify-center min-w-full pt-4 pb-1 md:pt-12 `}
 `;
 
-const ABoutContainer = styled.div`
+const InnerContainer = styled.div`
+  ${tw`flex flex-col w-full h-full max-w-screen-2xl`}
+`;
+
+const AboutContainer = styled.div`
   ${tw`flex flex-col `}
+`;
+
+const AboutText = styled.p`
+  ${tw`max-w-xs mt-2 text-sm font-normal leading-5 text-white`}
 `;
 
 export function Footer() {
   return (
     <FooterContainer>
-      <ABoutContainer>
-        <Logo color="white" bgColor="dark"></Logo>
-      </ABoutContainer>
+      <InnerContainer>
+        <AboutContainer>
+          <Logo color="white" bgColor="dark"></Logo>
+          <AboutText>YourCar is a car renting and selling company</AboutText>
+        </AboutContainer>
+      </InnerContainer>
     </FooterContainer>
   );
 }
