cf1d4a8f0fd6931c128d47ed4b37d9cfea916149
Zidane Innis <zidaneinnis@gmail.com>
2022-10-01 11:51:55 -0500

    Implemented white and dark them  logos for footer and header

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -1,11 +1,24 @@
 import React from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
+import Logo from "../logo";
 
 const FooterContainer = styled.div`
-  ${tw``}
+  min-height: 30em;
+  background-color: rgb(31 42 76);
+  ${tw`flex flex-row flex-wrap min-w-full pb-1 pt-7`}
+`;
+
+const ABoutContainer = styled.div`
+  ${tw`flex flex-col `}
 `;
 
 export function Footer() {
-  return <div>index</div>;
+  return (
+    <FooterContainer>
+      <ABoutContainer>
+        <Logo color="white" bgColor="dark"></Logo>
+      </ABoutContainer>
+    </FooterContainer>
+  );
 }
--- src/app/components/logo/index.tsx
+++ src/app/components/logo/index.tsx
@@ -4,13 +4,21 @@ import tw from "twin.macro";
 
 import CarLogoImg from "../../../assets/images/car-logo.png";
 
+import CarLogoDarkImg from "../../../assets/images/car-logo-dark.png";
+interface ILogoProps {
+  color?: "white" | "dark";
+  bgColor?: "white" | "dark";
+}
+
 const LogoContainer = styled.div`
   ${tw`flex items-center `}
 `;
 
 const LogoText = styled.div`
   ${tw`m-1 text-xl font-bold text-black md:text-2xl`}
-`;
+
+  ${({ color }: any) => (color === "white" ? tw`text-white` : tw`text-black`)}
+` as any;
 
 const Image = styled.div`
   width: auto;
@@ -22,13 +30,14 @@ const Image = styled.div`
   }
 `;
 
-function Logo() {
+function Logo(props: ILogoProps) {
+  const { color, bgColor } = props;
   return (
     <LogoContainer>
       <Image>
-        <img src={CarLogoImg}></img>
+        <img src={bgColor === "dark" ? CarLogoDarkImg : CarLogoImg}></img>
       </Image>
-      <LogoText>YourCar</LogoText>
+      <LogoText color={color || "dark"}>YourCar</LogoText>
     </LogoContainer>
   );
 }
