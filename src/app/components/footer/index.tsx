a9d8e1055106bd6875685e43ac4997d01e1a8266
Zidane Innis <zidaneinnis@gmail.com>
2022-10-06 00:03:51 -0500

    creating designing app footer

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -10,27 +10,34 @@ const FooterContainer = styled.div`
 `;
 
 const InnerContainer = styled.div`
-  ${tw`flex flex-col w-full h-full max-w-screen-2xl`}
+  ${tw`flex w-full h-full max-w-screen-2xl`}
 `;
 
 const AboutContainer = styled.div`
-  ${tw`flex flex-col `}
+  ${tw`flex flex-col mr-2 ml-7 md:mr-12`}
 `;
 
 const AboutText = styled.p`
   ${tw`max-w-xs mt-2 text-sm font-normal leading-5 text-white`}
 `;
 
+const SectionContainer = styled.div`
+  ${tw`flex flex-col mr-2 md:mr-12`}
+`;
+
 const LinksList = styled.div`
   ${tw`flex flex-col list-none outline-none`}
 `;
 
-const HeaderTitle = styled.ul`
-  ${tw`mb-3 text-base font-bold text-white `}
+const ListItem = styled.li`
+  ${tw`mb-3 `};
+  & > a {
+    ${tw`text-xs text-white transition-all hover:text-gray-200`}
+  }
 `;
 
-const ListItem = styled.li`
-  ${tw`mb-3 text-base font-bold text-white`}
+const HeaderTitle = styled.ul`
+  ${tw`mb-3 text-2xl font-bold text-white `}
 `;
 export function Footer() {
   return (
@@ -40,6 +47,43 @@ export function Footer() {
           <Logo color="white" bgColor="dark"></Logo>
           <AboutText>YourCar is a car renting and selling company</AboutText>
         </AboutContainer>
+        <SectionContainer>
+          <HeaderTitle>Our links</HeaderTitle>
+          <LinksList>
+            <ListItem>
+              <a href="#">Home </a>
+            </ListItem>
+            <ListItem>
+              <a href="#">About Us </a>
+            </ListItem>
+            <ListItem>
+              <a href="#">Services</a>
+            </ListItem>
+            <ListItem>
+              <a href="#">Blog </a>
+            </ListItem>
+          </LinksList>
+        </SectionContainer>
+        <SectionContainer>
+          <HeaderTitle>Other links</HeaderTitle>
+          <LinksList>
+            <ListItem>
+              <a href="#">FAQ </a>
+            </ListItem>
+            <ListItem>
+              <a href="#">Contact Us </a>
+            </ListItem>
+            <ListItem>
+              <a href="#">Support</a>
+            </ListItem>
+            <ListItem>
+              <a href="#">Privacy Policy</a>
+            </ListItem>
+            <ListItem>
+              <a href="#">Terms &amp; Conditions </a>
+            </ListItem>
+          </LinksList>
+        </SectionContainer>
       </InnerContainer>
     </FooterContainer>
   );
