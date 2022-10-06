4dcbfd4a2f4afc6c1468b295b0695582420d780f
Zidane Innis <zidaneinnis@gmail.com>
2022-10-06 03:19:35 -0500

    Creating designing app footer

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -1,20 +1,34 @@
+import {
+  faEnvelope,
+  faPhone,
+  faPhoneAlt,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
 import Logo from "../logo";
 
 const FooterContainer = styled.div`
-  min-height: 30em;
-  background-color: rgb(31 42 76);
-  ${tw`flex items-center justify-center min-w-full pt-4 pb-1 md:pt-12 `}
+  /* min-height: 24em; */
+  background-color: rgb(29, 32, 36);
+  ${tw`flex flex-col items-center min-w-full pt-10 pb-1 md:pt-12 `}
 `;
 
 const InnerContainer = styled.div`
-  ${tw`flex w-full h-full max-w-screen-2xl`}
+  ${tw`flex flex-wrap justify-center w-full h-full max-w-screen-2xl`}
 `;
 
+const BottomContainer = styled.div`
+  ${tw`flex justify-start w-full max-w-screen-2xl`}
+`;
+
+const CopyrightText = styled.div`
+  font-size: 11px;
+  ${tw`text-gray-400`}
+`;
 const AboutContainer = styled.div`
-  ${tw`flex flex-col mr-2 ml-7 md:mr-12`}
+  ${tw`flex flex-col pl-10 pr-10 mr-2 md:mr-16`}
 `;
 
 const AboutText = styled.p`
@@ -22,7 +36,7 @@ const AboutText = styled.p`
 `;
 
 const SectionContainer = styled.div`
-  ${tw`flex flex-col mr-2 md:mr-12`}
+  ${tw`flex flex-col w-full pl-10 pr-10 mr-2 mt-7 md:mr-16 md:pr-3 md:mt-0 md:pl-3 md:w-auto`}
 `;
 
 const LinksList = styled.div`
@@ -38,6 +52,18 @@ const ListItem = styled.li`
 
 const HeaderTitle = styled.ul`
   ${tw`mb-3 text-2xl font-bold text-white `}
+`;
+
+const RedIcon = styled.span`
+  ${tw`flex items-center justify-center mr-2 text-base text-white bg-red-500 rounded-full w-9 h-9`}
+`;
+
+const HorizontalContainer = styled.div`
+  ${tw`flex items-center `}
+`;
+
+const Smalltext = styled.h6`
+  ${tw`text-sm text-white`}
 `;
 export function Footer() {
   return (
@@ -84,7 +110,30 @@ export function Footer() {
             </ListItem>
           </LinksList>
         </SectionContainer>
+        <SectionContainer>
+          <HeaderTitle>Call Now</HeaderTitle>
+          <HorizontalContainer>
+            <RedIcon>
+              <FontAwesomeIcon icon={faPhoneAlt} />
+            </RedIcon>
+            <Smalltext>+1-876-316-2975</Smalltext>
+          </HorizontalContainer>
+        </SectionContainer>
+        <SectionContainer>
+          <HeaderTitle>Email</HeaderTitle>
+          <HorizontalContainer>
+            <RedIcon>
+              <FontAwesomeIcon icon={faEnvelope} />
+            </RedIcon>
+            <Smalltext>info@yourcar.com</Smalltext>
+          </HorizontalContainer>
+        </SectionContainer>
       </InnerContainer>
+      <BottomContainer>
+        <CopyrightText>
+          Copyright 2022 YourCar. All rights reserved.
+        </CopyrightText>
+      </BottomContainer>
     </FooterContainer>
   );
 }
